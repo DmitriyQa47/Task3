4a014ff9c84644aec67b1b28b75e5f2b8ad7af17
Dmitriy Sikorskiy <sikorskiy_1997@list.ru>
2023-03-28 14:56:41 +0300

    ititial commit

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,68 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+class CallbackTest {
+    public String generateDate(int days) {
+
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    }
+
+    String planningDate = generateDate(4);
+
+
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Ростов-на-Дону");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(planningDate);
+        form.$("[data-test-id=name] input").setValue("Тестов Тест");
+        form.$("[data-test-id=phone] input").setValue("+78536521458");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+
+                .shouldBe(Condition.visible);
+
+
+    }
+
+    @Test
+    void shouldSubmitRequestWithList() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Са");
+        $x("//span[text()='Ростов-на-Дону']").click();
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(planningDate);
+        form.$("[data-test-id=name] input").setValue("Тестов Тест");
+        form.$("[data-test-id=phone] input").setValue("+76528531459");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+
+                .shouldBe(Condition.visible);
+
+    }
+
+}
+
